3739d6fb09d1d1712ee92c16cd8b171fb0748059
olegvladimirow <omcodo@gmail.com>
2017-05-05 20:13:24 +0300

    the 3

---
+++ multi-estate-done/README.md
@@ -0,0 +1,1 @@
+# olegvladimirow.github.io
---
+++ multi-estate-done/multi-estate/js/media.js
@@ -0,0 +1,16 @@
+$(function() {
+	var menuhe = $('.menu-navi').height(); // высота меню
+	var menu = menuhe+20; // прибавляем 20
+$("area[href*=#],a[href*=#]:not([href=#]):not([href^='#tab']):not([href^='#quicktab']):not([href^='#pane']):not([href *= '#inline'])").click(function() {
+if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+var target = $(this.hash);
+target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+if (target.length) {
+$('html,body').animate({
+scrollTop: target.offset().top - menu
+},1500);
+return false;
+}
+}
+});
+});
